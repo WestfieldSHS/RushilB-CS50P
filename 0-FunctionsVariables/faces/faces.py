cc1f65c1ca2cf0e2d63ff5e93d6f74e42b4b0519
RushilBali21 <rushil.bali@education.nsw.gov.au>
2026-02-12 23:35:02 +0000

    Faces and playback problems completed

---
+++ 0-FunctionsVariables/faces/faces.py
@@ -0,0 +1,3 @@
+message = input ("message: ")
+convert = message.replace(":)", "🙂").replace(":(", "🙁")
+print(convert)
---
+++ 0-FunctionsVariables/playback/playback.py
@@ -0,0 +1,6 @@
+def main():
+    text = input('Message: ')
+    new_text = text.replace(' ', '...')
+    print(new_text)
+
+main()
