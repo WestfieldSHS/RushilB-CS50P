098bbaac5d4aff3f32568e4198821c75a0368436
RushilBali21 <rushil.bali@education.nsw.gov.au>
2026-02-19 23:43:31 +0000

    PROBLEM 1.1 DONE

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,9 @@
+answer = input('Answer: ')
+if answer == '42':
+    print('Yes')
+elif answer == 'Forty Two':
+    print('Yes')
+elif answer == 'forty-two':
+    print('Yes')
+else:
+    print('No')
