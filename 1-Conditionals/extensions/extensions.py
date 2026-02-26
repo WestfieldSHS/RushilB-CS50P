a5ad1f00c3ea71948b30f398e05fa4a2bf49a3a8
RushilBali21 <rushil.bali@education.nsw.gov.au>
2026-02-26 23:41:11 +0000

    PROBLEM 1.3 COMPLETED

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,8 @@
+file = input("File name: ")
+
+if file.endswith(".jpg"):
+    print("image/jpeg")
+elif file.endswith(".pdf"):
+    print("application/pdf")
+else:
+    print("application/octet-stream")
